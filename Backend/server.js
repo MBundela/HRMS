6bdf392ef8290e51323641b760cd940e51219da7
MBundela <113464403+MBundela@users.noreply.github.com>
2026-02-10 00:10:10 +0530

    h1

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-// CORS FIX
+
 app.use(cors({
   origin: [
     "https://employee-xz5m.vercel.app",
@@ -22,18 +22,15 @@ app.use(cors({
 app.options("*", cors());
 app.use(express.json());
 
-// DB Connection
+
 connectDB();
 
-// Root Test
 app.get("/", (req, res) => {
   res.send("HRMS Backend Running Successfully 🚀");
 });
 
-// Routes
 app.use("/api/employees", employeeRoutes);
 app.use("/api/attendance", attendanceRoutes);
 
-// PORT FIX FOR RENDER
 const PORT = process.env.PORT || 4500;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
