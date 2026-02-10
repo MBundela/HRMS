0f6a1f249e79cb74fa16cae5b899d10725cf978e
MBundela <113464403+MBundela@users.noreply.github.com>
2026-02-10 14:56:55 +0530

    1

--- Backend/server.js
+++ Backend/server.js
@@ -8,18 +8,15 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-
 app.use(cors({
   origin: [
     "https://employee-xz5m.vercel.app",
     "http://localhost:5173"
   ],
-  methods: ["GET","POST","PUT","DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
-app.options("*", cors());
+// app.options("*", cors());
 
 app.use(express.json());
 
