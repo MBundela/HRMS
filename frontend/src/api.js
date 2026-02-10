f1cd66cf25cb38de0112f0b80282fb24028d912c
MBundela <113464403+MBundela@users.noreply.github.com>
2026-02-10 12:48:02 +0530

    1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
-export const API = "https://hrms-15rf.onrender.com/api";
+// export const API = "https://hrms-15rf.onrender.com/api";
 
-// export const API = import.meta.env.MODE === "development"
-//   ? "http://localhost:4500/api"
-//   : "https://hrms-15rf.onrender.com/api";
+export const API = import.meta.env.MODE === "development"
+  ? "http://localhost:4500/api"
+  : "https://hrms-15rf.onrender.com/api";
